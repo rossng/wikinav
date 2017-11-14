ebb478fe3bf5ec754790372b26034a0f664a8a22
Alessio Zakaria <alessio.b.zakaria@googlemail.com>
2017-11-14 15:21:21 +0000

    simple servent api

--- wikinav/src/Lib.hs
+++ wikinav/src/Lib.hs
@@ -38,11 +38,17 @@ import Text.Blaze.Html.Renderer.Utf8
 import qualified Data.Aeson.Parser
 import qualified Text.Blaze.Html
 
-type NotifyAPI = "wiki" :> Capture "url" :> POST `[JSON] Frequency
+data ThisDone = ThisDone String deriving (Eq, Show, Generic) 
+
+instance ToJSON ThisDone
+
+
+
+type NotifyAPI = "wiki" :> Capture "url" String :> Get '[JSON] ThisDone
 
 type WordAPI1 = "word" :> Get '[JSON] [Frequency]
 
-type API = NotifyAPI | WordAPI1
+type API = NotifyAPI :<|> WordAPI1
 
 type FreqMap = Map String Float
 
@@ -58,19 +64,20 @@ freqmap = Map.singleton "and" 1
 words1 :: [Frequency]
 words1 = [ Frequency freqmap]
 
-server1 :: Handler WordAPI1
+server1 :: Handler [Frequency]
 server1 = return words1
 
-server2 :: String -> Handler NotifyAPI
-server2 url = lift $ writeFile "current.txt" url
+server2 :: String -> Handler ThisDone
+server2 url = liftIO (writeFile "current.txt" url)   >> return (ThisDone "done")
 
-wordAPI :: Proxy WordAPI1
+wordAPI :: Proxy API
 wordAPI = Proxy
 
-server = server1 :<|> server2
+server :: Server API
+server = server2 :<|> server1 
 
 app1 :: Application
-app1 = serve wordAPI server1
+app1 = serve wordAPI server
 
 someFunc :: IO ()
 someFunc = run 8081 app1
